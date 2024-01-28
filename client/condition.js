39b7afa84893de771cf5ff7a0d1496785c5ab39d
koomin1227 <koomin1227@naver.com>
2024-01-29 03:29:23 +0900

    feat: 학습 시작 메서드 구현

--- client/condition.js
+++ client/condition.js
@@ -1,11 +1,11 @@
-const apiUrl = 'http://49.50.167.220:3001';
-
+// const apiUrl = 'http://49.50.167.220:3001';
+const apiUrl = 'http://localhost:3001/condition';
 async function updateCondition() {
   const condition = await getCondition();
   changeConditionDisplay(condition);
 }
 async function getCondition() {
-  const res = await fetch(apiUrl + '/condition');
+  const res = await fetch(apiUrl);
   const body = await res.json();
   return body.condition;
 }
@@ -22,4 +22,38 @@ function changeConditionDisplay(condition) {
   }
 }
 
-export { updateCondition };
+async function startLearning(name) {
+  const res = await fetchLearningStart(name);
+
+  if (res.ok) {
+    await updateCondition();
+    alert('학습시작');
+  } else {
+    const body = await res.json();
+    if (body.message === '1') {
+      alert(`${name}님은 이미 학습중입니다.`);
+    } else if (body.message === '2') {
+      alert(`다른 사람이 이미 학습중입니다.`);
+    } else {
+      alert(`오류`);
+    }
+  }
+}
+
+async function fetchLearningStart(name) {
+  return await fetch(apiUrl + '/start', {
+    method: 'POST',
+    headers: {
+      'Content-Type': 'application/json',
+    },
+    body: JSON.stringify({
+      name,
+    }),
+  });
+}
+
+// fetchLearningStart('이광훈').then(async (res) =>
+//   console.log(res.status, await res.json()),
+// );
+
+export { updateCondition, startLearning };
