03ff1ce74644ec42970ab2b1419d7b9d0aee78d6
koomin1227 <koomin1227@naver.com>
2024-01-29 00:42:34 +0900

    feat: Condition controller 구현

--- src/condition/condition.controller.ts
+++ src/condition/condition.controller.ts
@@ -1,4 +1,22 @@
-import { Controller } from '@nestjs/common';
+import { Body, Controller, Get, Post } from '@nestjs/common';
+import { ConditionService } from './condition.service';
+import { ConditionDto } from './condition.dto';
 
 @Controller('condition')
-export class ConditionController {}
+export class ConditionController {
+  constructor(private conditionService: ConditionService) {}
+  @Get()
+  conditionDetail() {
+    return this.conditionService.getCondition();
+  }
+
+  @Post('/start')
+  conditionStart(@Body() body: ConditionDto) {
+    this.conditionService.startLearning(body.name);
+  }
+
+  @Post('/end')
+  conditionEnd(@Body() body: ConditionDto) {
+    this.conditionService.endLearning(body.name);
+  }
+}
