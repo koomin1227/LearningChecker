c7d066666f4097ba07f2ed5c5d88c04d7a6c2052
koomin1227 <koomin1227@naver.com>
2024-01-29 03:29:46 +0900

    feat: 학습 시작 버튼을 누르면 학습시작하는 기능 구현

--- client/main.js
+++ client/main.js
@@ -1,5 +1,15 @@
-import { updateCondition } from './condition.js';
+import { startLearning, updateCondition } from './condition.js';
 
 document.addEventListener('DOMContentLoaded', async function () {
   await updateCondition();
+  document.getElementById('refresh').addEventListener('click', async () => {
+    await updateCondition();
+  });
+  document.querySelectorAll('.start_button').forEach((button) => {
+    button.addEventListener('click', async () => {
+      const name =
+        button.parentNode.parentNode.querySelector('.name').textContent;
+      await startLearning(name);
+    });
+  });
 });
