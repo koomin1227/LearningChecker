47795a2e0b078cff149a539de4fb1536aec5938e
koomin1227 <koomin1227@naver.com>
2024-01-28 23:59:38 +0900

    feat: 강의 수강 현황을 반환하는 함수 구현

--- src/condition/condition.service.ts
+++ src/condition/condition.service.ts
@@ -1,4 +1,12 @@
 import { Injectable } from '@nestjs/common';
+import { ConditionRepository } from './condition.repository';
 
 @Injectable()
-export class ConditionService {}
+export class ConditionService {
+  constructor(private conditionRepository: ConditionRepository) {}
+
+  getCondition() {
+    const condition = this.conditionRepository.find();
+    return { condition };
+  }
+}
