60b8055f002015e3784c9fe67a2c0a8c44d701a3
koomin1227 <koomin1227@naver.com>
2024-01-29 00:47:16 +0900

    feat: provider 등록

--- src/condition/condition.module.ts
+++ src/condition/condition.module.ts
@@ -1,9 +1,11 @@
 import { Module } from '@nestjs/common';
 import { ConditionController } from './condition.controller';
 import { ConditionService } from './condition.service';
+import { MemberRepository } from './member.repository';
+import { ConditionRepository } from './condition.repository';
 
 @Module({
   controllers: [ConditionController],
-  providers: [ConditionService],
+  providers: [ConditionService, MemberRepository, ConditionRepository],
 })
 export class ConditionModule {}
