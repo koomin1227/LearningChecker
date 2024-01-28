17bf92319ff63ff3c78a48b40d2bdc72409b5f07
koomin1227 <koomin1227@naver.com>
2024-01-29 00:14:42 +0900

    feat: 학습을 시작하는 기능 구현

--- src/condition/condition.service.ts
+++ src/condition/condition.service.ts
@@ -1,12 +1,35 @@
-import { Injectable } from '@nestjs/common';
+import { HttpException, Injectable } from '@nestjs/common';
 import { ConditionRepository } from './condition.repository';
+import { MemberRepository } from './member.repository';
+import { NotFoundError } from 'rxjs';
+
+interface Condition {
+  condition: string;
+}
 
 @Injectable()
 export class ConditionService {
-  constructor(private conditionRepository: ConditionRepository) {}
+  constructor(
+    private conditionRepository: ConditionRepository,
+    private memberRepository: MemberRepository,
+  ) {}
 
-  getCondition() {
+  getCondition(): Condition {
     const condition = this.conditionRepository.find();
     return { condition };
+  }
+
+  startLearning(name: string) {
+    if (!this.memberRepository.findOne(name)) {
+      throw new NotFoundError('not exist');
+    }
+    const condition = this.conditionRepository.find();
+    if (condition == name) {
+      throw new HttpException('1', 400); // 본인이 학습중인데 시작
+    }
+    if (condition != null) {
+      throw new HttpException('2', 400); // 다른 사람이 학습중
+    }
+    this.conditionRepository.update(name);
   }
 }
