eb8d8e793011f1fd6bb8dbc0cccd6fe41c6ff767
koomin1227 <koomin1227@naver.com>
2024-01-28 23:51:27 +0900

    feat: condition.repository.ts 생성

---
+++ src/condition/condition.repository.ts
@@ -0,0 +1,8 @@
+export class ConditionRepository {
+  private member: string[];
+  private condition: string;
+  constructor() {
+    this.member = ['구민', '이광훈'];
+    this.condition = null;
+  }
+}
