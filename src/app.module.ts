e10e8309420228a25413c7489a29eede0868d1e1
koomin1227 <koomin1227@naver.com>
2024-01-28 23:44:55 +0900

    feat: condition 모듈 생성

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,10 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { ConditionModule } from './condition/condition.module';
 
 @Module({
-  imports: [],
+  imports: [ConditionModule],
   controllers: [AppController],
   providers: [AppService],
 })
