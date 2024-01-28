e17ec382cdf900ade555728837f576b5bea2f60e
koomin1227 <koomin1227@naver.com>
2024-01-29 00:18:52 +0900

    feat: 학습을 종료하는 기능 구현

--- src/condition/condition.service.ts
+++ src/condition/condition.service.ts
@@ -32,4 +32,15 @@ export class ConditionService {
     }
     this.conditionRepository.update(name);
   }
+
+  endLearning(name: string) {
+    if (!this.memberRepository.findOne(name)) {
+      throw new NotFoundError('not exist');
+    }
+    const condition = this.conditionRepository.find();
+    if (condition != name) {
+      throw new HttpException('3', 400); // 본인이 학습중이지 않은데 종료
+    }
+    this.conditionRepository.update(null);
+  }
 }
