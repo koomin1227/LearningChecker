38f63608371c5060e19233f900378249485682b1
koomin1227 <koomin1227@naver.com>
2024-02-12 23:58:36 +0900

    feat: 학습 경과 시간 2시간 간격으로 알림 전송

--- src/condition/condition.repository.ts
+++ src/condition/condition.repository.ts
@@ -1,16 +1,26 @@
 export class ConditionRepository {
   private learner: string;
   private updateTime: Date;
+  private elapseTime: number;
   constructor() {
     this.learner = null;
   }
 
   find() {
-    return { learner: this.learner, updateTime: this.updateTime };
+    return {
+      learner: this.learner,
+      updateTime: this.updateTime,
+      elapseTime: this.elapseTime,
+    };
   }
 
   update(name: string) {
     this.learner = name;
     this.updateTime = new Date();
+    this.updateTime = null;
+  }
+
+  updateElapseTime(hour: number) {
+    this.elapseTime = hour;
   }
 }
--- src/condition/condition.service.ts
+++ src/condition/condition.service.ts
@@ -1,6 +1,8 @@
 import { HttpException, Injectable } from '@nestjs/common';
 import { ConditionRepository } from './condition.repository';
 import { MemberRepository } from './member.repository';
+import { Cron } from '@nestjs/schedule';
+import { AlarmService } from './alarm.service';
 
 export interface Condition {
   condition: string;
@@ -11,6 +13,7 @@ export class ConditionService {
   constructor(
     private conditionRepository: ConditionRepository,
     private memberRepository: MemberRepository,
+    private alarmService: AlarmService,
   ) {}
 
   getCondition(): Condition {
@@ -43,5 +46,31 @@ export class ConditionService {
     if (!this.memberRepository.findOne(name)) {
       throw new HttpException('not exist', 404);
     }
+  }
+
+  @Cron('* * * * *')
+  async checkUserUsage() {
+    const condition = this.conditionRepository.find();
+    if (condition.learner == null) {
+      return;
+    }
+    const hoursDifference = this.getTimeDifference(condition.updateTime);
+    if (
+      hoursDifference % 2 == 0 &&
+      condition.elapseTime != hoursDifference &&
+      hoursDifference >= 2
+    ) {
+      this.conditionRepository.updateElapseTime(hoursDifference);
+      await this.alarmService.sendMessage(
+        `${condition.learner}님 ${hoursDifference} 시간 지났습니다.`,
+      );
+    }
+  }
+
+  getTimeDifference(time) {
+    const currentTime = new Date();
+    const timeDifference = currentTime.getTime() - time.getTime();
+    const hoursDifference = timeDifference / (1000 * 60 * 60);
+    return Math.floor(hoursDifference);
   }
 }
